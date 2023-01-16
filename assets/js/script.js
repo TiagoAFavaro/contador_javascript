78b5fc2af99184ebb05a32ae5973a2633b8c7799
Tiago <tiago.favaro@hotmail.com>
2023-01-16 16:38:45 -0300

    primeiro commit - contador

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var currentNumberWrapper = document.getElementById('currentNumber')
+var currentNumber = 0;
+
+function increment() {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement() {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+    if (currentNumber.count > 10);
+        document.getElementById("currentNumber").disable = true;
